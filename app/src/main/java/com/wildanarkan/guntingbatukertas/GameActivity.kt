4a294467d3bd108d55676f6441e5cf95a8a71551
wildanarkan <cillmystic@gmail.com>
2023-12-19 11:50:29 +0700

    Unit Test

---
+++ app/src/main/java/com/wildanarkan/guntingbatukertas/GameActivity.kt
@@ -0,0 +1,12 @@
+package com.wildanarkan.guntingbatukertas
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+
+class GameActivity : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        
+    }
+}
